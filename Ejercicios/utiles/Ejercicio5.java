b67b4eed5fc95eae2865666c44770a5ba81813c0
Brais <103991765+BraisSO@users.noreply.github.com>
2022-09-14 12:25:42 +0200

    ejercicios

---
+++ Ejercicios/utiles/Ejercicio5.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Ejercicio5 {
+	
+	public void comprobadorPares(int numero) {
+		if (numero%2==0) {
+			System.out.println("El numero es par");
+		}
+		
+		else {
+			System.out.println("El numero es impar");
+		}
+	}
+
+}
